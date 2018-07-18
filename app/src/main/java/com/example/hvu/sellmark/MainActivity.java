b5c172f8e9538b0951ef5758f51bbb8d94383570
Hai Vu <hvu@sellmark.net>
2018-07-18 15:10:49 -0500

    login page

---
+++ app/src/main/java/com/example/hvu/sellmark/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.hvu.sellmark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtusername, edtpassword;
+    Button btnlogin;
+    String urlvalidate = "http://10.0.0.201/~hvu/androidwebservice/validate_login.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapped();
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edtusername.getText().toString();
+                String password = edtpassword.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Enter Username/Password", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    validatelogin(urlvalidate);
+                }
+            }
+        });
+
+    }
+
+    private void validatelogin(String url){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.trim().equals(edtusername.getText().toString())){
+                            Toast.makeText(MainActivity.this, "good", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "bad", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error", error.toString());
+                    }
+                }
+        ){
+            // get values from user input and pass to method POST php
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", edtusername.getText().toString().trim());
+                params.put("password", edtpassword.getText().toString().trim());
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    public void mapped(){
+        edtusername = (EditText) findViewById(R.id.edtusername);
+        edtpassword = (EditText) findViewById(R.id.edtpassword);
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+    }
+}
---
+++ app/src/main/java/com/example/hvu/sellmark/index_page.java
@@ -0,0 +1,13 @@
+package com.example.hvu.sellmark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class index_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index_page);
+    }
+}
